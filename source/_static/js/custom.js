c3329f82fce1a099438917c8a9bcfb590f7c2e19
Benjamin Berhault <benjamin.berhault@hotmail.com>
2020-04-09 10:08:29 +0200

    Google Analytics setup

---
+++ source/_static/js/custom.js
@@ -0,0 +1,4 @@
+if(location.hostname == "ds4es.org"){
+	// Global site tag (gtag.js) - Google Analytics
+    document.write("<script async src='https://www.googletagmanager.com/gtag/js?id=UA-163235556-1'></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-163235556-1');</script>");
+}
