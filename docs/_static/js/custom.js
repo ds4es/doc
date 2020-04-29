6128011a03a98fa0ebe76016ed9cc11ff10090c0
Benjamin Berhault <benjamin.berhault@hotmail.com>
2020-04-29 09:02:59 +0200

    Google analytics update

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,7 +1,7 @@
 
 // Scripts in document.write() are loaded when requested 
 // from ds4es.org 
-if(location.hostname == "ds4es.org"){
+if(location.hostname == "ds4es.org/docs"){
 	// Global site tag (gtag.js) - Google Analytics
     document.write("<script async src='https://www.googletagmanager.com/gtag/js?id=UA-163235556-1'></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-163235556-1');</script>");
 }
